764218241545063b85362b0d98f722bf3f41ccb3
Justin Freeman <justinfreeman@Justins-iMac.attlocal.net>
2025-01-03 13:02:29 -0500

    Completion

---
+++ db-connection.js
@@ -0,0 +1,4 @@
+const fetch = require('node-fetch');
+const mongoose = require("mongoose");
+const dbUri = 'mongodb+srv://justinfreeman066:6IH9spkzP4JKM2UN@cluster0.grvyp.mongodb.net/';
+mongoose.connect(dbUri);
